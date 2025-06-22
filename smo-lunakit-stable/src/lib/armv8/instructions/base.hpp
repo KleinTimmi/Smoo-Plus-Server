eaa7fd5c694fa1039ded77f19b90eeb5779a8ac9
KleinTimmi <zwergel19@outlook.de>
2025-06-22 08:05:25 +0200

    a

--- smo-lunakit-stable/src/lib/armv8/instructions/base.hpp
+++ smo-lunakit-stable/src/lib/armv8/instructions/base.hpp
@@ -2,39 +2,22 @@
 
 #include <lib/armv8.hpp>
 
-namespace exl::armv8::inst {
+namespace exl::armv8::inst::impl {
 
-    struct Instruction : public InstBitSet {
+    struct Opx1x0Instruction : public Instruction {
 
-        ACCESSOR(MainOp0, 25, 29);
+        ACCESSOR(Op0, 28, 32);
+        ACCESSOR(Op1, 26);
+        ACCESSOR(Op2, 23, 25);
+        ACCESSOR(Op3, 16, 22);
+        ACCESSOR(Op4, 10, 12);
 
-        constexpr Instruction(u8 op0) {
-            SetMainOp0(op0);
+        constexpr Opx1x0Instruction(u8 op0) : Instruction(0b0100) {
+            SetOp0(op0);
         }
     };
-
-    enum ShiftType : u8 {
-        ShiftType_LSL = 0b00,
-        ShiftType_LSR = 0b01,
-        ShiftType_ASR = 0b10,
-        ShiftType_ROR = 0b11,
-    };
-
-    enum ExtendType : u8 {
-        ExtendType_UXTB = 0b000, 
-        ExtendType_UXTH = 0b001, 
-        ExtendType_UXTW = 0b010, 
-        ExtendType_UXTX = 0b011, 
-        ExtendType_LSL  = 0b011, 
-        ExtendType_SXTB = 0b100, 
-        ExtendType_SXTH = 0b101, 
-        ExtendType_SXTW = 0b110, 
-        ExtendType_SXTX = 0b111, 
-    };
-
 }
 
-#include "op100x/base.hpp"
-#include "op101x/base.hpp"
-#include "opx1x0/base.hpp"
-#include "opx101/base.hpp"
+#include "load_store_register_offset/base.hpp"
+#include "load_store_register_unscaled_immediate/base.hpp"
+#include "load_store_register_unsigned_immediate/base.hpp"
--- smo-lunakit-stable/src/types.h
+++ smo-lunakit-stable/src/types.h
@@ -1,69 +1,100 @@
-/**
- * @file switch/types.h
- * @brief Various system types.
- * @copyright libnx Authors
- */
 #pragma once
 
-#include "../../types.h"
-
 #include <stddef.h>
-#include <stdalign.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <limits.h>
 
-#include "result.h"
+typedef uint8_t u8;       ///<   8-bit unsigned integer.
+typedef uint16_t u16;     ///<  16-bit unsigned integer.
+typedef uint32_t u32;     ///<  32-bit unsigned integer.
+typedef uint64_t u64;     ///<  64-bit unsigned integer.
+typedef __uint128_t u128; ///< 128-bit unsigned integer.
 
-/// The maximum value of a u64.
-#define U64_MAX	UINT64_MAX
+typedef int8_t s8;       ///<   8-bit signed integer.
+typedef int16_t s16;     ///<  16-bit signed integer.
+typedef int32_t s32;     ///<  32-bit signed integer.
+typedef int64_t s64;     ///<  64-bit signed integer.
+typedef __int128_t s128; ///< 128-bit unsigned integer.
 
-#ifndef SSIZE_MAX
-#ifdef SIZE_MAX
-#define SSIZE_MAX ((SIZE_MAX) >> 1)
-#endif
-#endif
+typedef volatile u8 vu8;     ///<   8-bit volatile unsigned integer.
+typedef volatile u16 vu16;   ///<  16-bit volatile unsigned integer.
+typedef volatile u32 vu32;   ///<  32-bit volatile unsigned integer.
+typedef volatile u64 vu64;   ///<  64-bit volatile unsigned integer.
+typedef volatile u128 vu128; ///< 128-bit volatile unsigned integer.
 
-typedef u32 Handle;                 ///< Kernel object handle.
-typedef u32 Result;                 ///< Function error code result type.
-typedef void (*ThreadFunc)(void *); ///< Thread entrypoint function.
-typedef void (*VoidFn)(void);       ///< Function without arguments nor return value.
+typedef volatile s8 vs8;     ///<   8-bit volatile signed integer.
+typedef volatile s16 vs16;   ///<  16-bit volatile signed integer.
+typedef volatile s32 vs32;   ///<  32-bit volatile signed integer.
+typedef volatile s64 vs64;   ///<  64-bit volatile signed integer.
+typedef volatile s128 vs128; ///< 128-bit volatile signed integer.
 
-/// Creates a bitmask from a bit number.
-#ifndef BIT
-#define BIT(n) (1U<<(n))
-#endif
+typedef unsigned char   uchar;
+typedef	unsigned short	ushort;
+typedef	unsigned int	uint;	
+typedef	unsigned long	ulong;
 
-/// Packs a struct so that it won't include padding bytes.
-#ifndef PACKED
-#define PACKED     __attribute__((packed))
-#endif
+typedef unsigned char   undefined;
+typedef unsigned char    undefined1;
+typedef unsigned short    undefined2;
+typedef unsigned int    undefined3;
+typedef unsigned int    undefined4;
+typedef unsigned long    undefined8;
 
-/// Marks a function as not returning, for the purposes of compiler optimization.
-#ifndef NORETURN
-#define NORETURN   __attribute__((noreturn))
-#endif
+#define ALIGN_UP(x, a) ((((uintptr_t)x) + (((uintptr_t)a)-1)) & ~(((uintptr_t)a)-1))
+#define ALIGN_DOWN(x, a) ((uintptr_t)(x) & ~(((uintptr_t)(a)) - 1))
+#define ALIGNED(a)      __attribute__((aligned(a)))
+#define ON_INIT         __attribute__((constructor))
+#define NOINLINE        __attribute__((noinline))
+#define NORETURN        __attribute__((noreturn))
+#define PACKED       __attribute__((packed))
+#define UNREACHABLE __builtin_unreachable()
+#define PAGE_SIZE (0x1000)
+#define ALWAYS_INLINE inline __attribute__((always_inline))
+#define BITSIZEOF(x) (sizeof(x) * CHAR_BIT)
 
-/// Performs a dummy operation on the specified argument in order to silence compiler warnings about unused arguments.
-#ifndef IGNORE_ARG
-#define IGNORE_ARG(x) (void)(x)
-#endif
+// from: https://www.scs.stanford.edu/~dm/blog/va-opt.html
 
-/// Flags a function as deprecated.
-#ifndef DEPRECATED
-#ifndef LIBNX_NO_DEPRECATION
-#define DEPRECATED __attribute__ ((deprecated))
-#else
-#define DEPRECATED
-#endif
-#endif
+// Rescan macro tokens 256 times
+#define EXPAND(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) __VA_ARGS__
 
-/// Flags a function as (always) inline.
-#define NX_INLINE __attribute__((always_inline)) static inline
+#define PARENS ()
 
-/// Flags a function as constexpr in C++14 and above; or as (always) inline otherwise.
-#if __cplusplus >= 201402L
-#define NX_CONSTEXPR NX_INLINE constexpr
-#else
-#define NX_CONSTEXPR NX_INLINE
-#endif
+#define FOR_EACH(macro, ...)                                    \
+  __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...)                         \
+  macro(a1)                                                     \
+  __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
 
-/// Invalid handle.
-#define INVALID_HANDLE ((Handle) 0)
+typedef __builtin_va_list va_list;
+#define va_start(v,l) __builtin_va_start(v,l)
+#define va_end(v) __builtin_va_end(v)
+
+#define RAD(deg) (deg * (M_PI / 180)) // converts Degrees to Radians
+#define DEG(rad) (rad * (180 / M_PI)) // converts Radians to Degrees
+#define BTOC(bool) (bool ? "true" : "false") // converts boolean to true/false char
+#define ACNT(arr) (sizeof(arr) / sizeof(arr[0]))  // returns size of inputted array
+// used to convert macro values to strings
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+//using u64 = std::uint64_t;
+//using s64 = std::int64_t;
+
+// stores a result on a lot of OS-related functions
+typedef u32 Result;
+typedef u32 Handle;
+typedef void (*ThreadFunc)(void*);
+
+// socket types
+
+typedef uint16_t in_port_t;
+typedef uint32_t in_addr_t;
+typedef uint16_t sa_family_t;
+
+typedef unsigned int socklen_t;
